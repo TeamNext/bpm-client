ddd5ca1bdf764b8d6009e5c6a729a0cab30e9519
Tao Wen <taowen@gmail.com>
2013-08-12 17:48:38 +0800

    add auto-flow demo

--- container/urls.py
+++ container/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.shortcuts import render
 
 admin.autodiscover()
 
@@ -8,5 +9,8 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^django-rq/', include('django_rq.urls')),
-    url(r'^', include('bpm.webservice.urls'))
+    url(r'^', include('bpm.webservice.urls')),
+    url(r'^flowchart-demo/(?P<task_class_name>.*)/$',
+        lambda x, task_class_name: render(x, 'flowchart_demo/auto_flow.html',
+                                          {'task_class_name':task_class_name})),
 )
---
+++ static/auto_flow.js
@@ -0,0 +1,127 @@
+var _fc_style = "<style>.stage {margin-bottom: 8px;}\
+        	.task {padding-top: 8px;width: 128px;height: 64px;background: lightblue;\
+            		margin-left: 8px;margin-top: 8px;margin-bottom: 8px;\
+            		display: -moz-inline-stack;display: inline-block;*display: inline;\
+            		zoom: 1;text-align: center;vertical-align: middle;}</style>"
+
+var _fc_width = 320; //total width for flowchart
+
+/* example data
+var _fc_data = [
+		[
+			{description:"ABC", name:1, predecessors:[]}
+		], //stage1
+		[
+			{description:"DEF", name:2, predecessors:[1]},
+			{description:"GHI", name:3, predecessors:[1]}
+		], //stage2
+		[
+			{description:"JKL", name:4, predecessors:[2,3]}
+		], //stage3
+	]
+*/
+
+//generate a static flowchart, according to flowchart json
+//	1) get data
+//		data source: flowchart api
+//  2) generate task box div
+//  3) jsplumb connect boxes
+function gen_static_flowchart(task_name, $chart_container){
+	var _fc_html = $('<div style="width: ' + _fc_width + 'px">')
+	//ajax request
+    $.ajax({
+        url: '/v1/flowcharts/' + task_name + '/',
+        type: 'GET',
+        dataType: 'json',
+        //data:
+        success: function(_fc_data){
+            //draw stages
+            $.each(_fc_data, function(_stage_idx, _stage){
+                var _stage_html = $('<div class="stage">')
+                //draw steps
+                $.each(_stage, function(_step_idx, _step){
+                    var _step_html = $('<div class="task">')
+                                .attr('id', '_fc_step_' + _step.name)
+                                .attr('style', 'width:' + _fc_width/_stage.length + 'px')
+                                .html(_step.description)
+                    _stage_html.append(_step_html)
+                })  
+
+                _fc_html.append(_stage_html)
+            })
+
+            $chart_container.html(_fc_style + _fc_html.html());
+
+
+            jsPlumb.ready(function () {
+                jsPlumb.importDefaults({
+        //              // default to blue at one end and green at the other
+        //              EndpointStyles : [{ fillStyle:'#225588' }, { fillStyle:'#558822' }],
+        //              // blue endpoints 7 px; green endpoints 11.
+        //              Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]],
+                    // the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+                    // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+        //              ConnectionOverlays : [
+        //                  [ "Arrow", { location: 0, width:12, height:8} ]
+        //              ]
+                });
+
+                var connectorPaintStyle = {
+                    lineWidth: 2,
+                    strokeStyle: "#c0c0c0",
+                    joinstyle: "round"
+                };
+                // .. and this is the hover style.
+                var connectorHoverStyle = {
+                    lineWidth: 4,
+                    strokeStyle: "#2e2aF8"
+                };
+                var endpointHoverStyle = {fillStyle: "#2e2aF8"};
+                // the definition of source endpoints (the small blue ones)
+                var sourceEndpoint = {
+                    endpoint: "Blank",
+                    isSource: true,
+                    connector: [ "Flowchart", { stub: [0, 0], gap: 0, cornerRadius: 40, alwaysRespectStubs: true } ],
+                    connectorStyle: connectorPaintStyle
+        //              hoverPaintStyle:endpointHoverStyle,
+        //              connectorHoverStyle:connectorHoverStyle,
+                };
+                var targetEndpoint = {
+                    endpoint: ['Image', {src: '/static/down_arrow.png', hoverClass: 'hover-endpoint', width: 16, height: 16}],
+                    paintStyle: {
+                        fillStyle: "blue"
+                    },
+        //              hoverPaintStyle:endpointHoverStyle,
+                    dropOptions: { hoverClass: "hover", activeClass: "active" },
+                    isTarget: true
+                };
+                jsPlumb.Defaults.Container = $('body');
+                function connect(src, target) {
+                    var src = jsPlumb.addEndpoint(src, sourceEndpoint, { anchor: 'BottomCenter' });
+                    var target = jsPlumb.addEndpoint(target, targetEndpoint, { anchor: 'TopCenter' });
+                    jsPlumb.connect({ source: src, target: target });
+                }
+
+
+                $.each(_fc_data, function(_stage_idx, _stage){
+                    $.each(_stage, function(_step_idx, _step){
+                        $.each(_step.predecessors, function(_pre_idx, _pre){
+                            connect($('#_fc_step_' + _pre), $('#_fc_step_' + _step.name))
+                        })
+                    })  
+                })
+
+            });
+
+            
+        }
+    });
+}
+
+
+//render status of each task onto the static flowchart
+//find boxes by name
+//data source: trace api
+function render_state(){
+
+}
