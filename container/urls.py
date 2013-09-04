ebed38925c9c4a60093e6bbed5dfb49c5d33c810
Tao Wen <taowen@gmail.com>
2013-09-05 00:22:50 +0800

    fix webservice

--- container/urls.py
+++ container/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^django-rq/', include('django_rq.urls')),
-    # url(r'^', include('bpm.webservice.urls')),
+    url(r'^', include('bpm.webservice.urls')),
 )
