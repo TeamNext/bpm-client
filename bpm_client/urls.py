ae2276e7ac58e65e9d929d2a6d9bef1b61ab55ba
Tao Wen <taowen@gmail.com>
2013-10-18 15:42:40 +0800

    remove unused code

--- bpm_client/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns
-
-urlpatterns = patterns(
-    'bpm_client.views',
-    (r'^flowchart/task-definition/(?P<task_class_name>.*)/$', 'task_definition_flowchart'),
-    (r'^flowchart/task/(?P<task_id>.*)/$', 'task_flowchart'),
-)
--- bpm_client/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def task_definition_flowchart(request, task_class_name):
-    return render(request, 'flowchart/task-definition.html', {'task_class_name': task_class_name})
-
-
-def task_flowchart(request, task_id):
-    return render(request, 'flowchart/task.html', {'task_id': task_id})
