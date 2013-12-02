566857ec38ab8fbb5b34d251c901035e460ba305
Tao Wen <taowen@gmail.com>
2013-12-02 14:24:31 +0800

    enable qos

--- container/shared_settings.py
+++ container/shared_settings.py
@@ -263,3 +263,5 @@ PASSPORT_SERVICE_SIGNOUT_URL = 'http://passport.oa.com/modules/passport/signout.
 PASSPORT_SERVICE_WSDL = 'http://passport.oa.com/services/passportservice.asmx?WSDL'
 AUTHENTICATION_BACKENDS = ('bpm.contrib.auth.backends.TicketBackend',)
 ANONYMOUS_USER_ID = -1
+BPM_JOB_WORKER = 'bpm.workers.qos.QoSBackend'
+QOS_URL = 'http://127.0.0.1:8080/'
